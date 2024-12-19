a40db5aa6046d727017c97faefdd316fb277eaa2
lxf <infiniment@163.com>
2024-12-19 09:49:01 +0800

    1.2.0

---
+++ rollup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'rollup'
+import typescript from '@rollup/plugin-typescript'
+
+export default defineConfig({
+  input: 'src/index.ts',
+  output: [
+    {
+      file: './index.js',
+      format: 'umd',
+      name: 'Cookies',
+      noConflict: true,
+      banner: ';'
+    }
+  ],
+  plugins: [
+    typescript()
+  ]
+})
